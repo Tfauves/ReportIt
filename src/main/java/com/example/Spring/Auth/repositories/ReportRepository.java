dc93e80b18574a36d2343c208b9352d088ab0330
Tranquility Base <tfauvelle@gmail.com>
2022-04-04 13:31:45 -0400

    custom query

--- src/main/java/com/example/Spring/Auth/repositories/ReportRepository.java
+++ src/main/java/com/example/Spring/Auth/repositories/ReportRepository.java
@@ -2,6 +2,12 @@ package com.example.Spring.Auth.repositories;
 
 import com.example.Spring.Auth.models.report.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    @Query(value = "SELECT * FROM report WHERE profile_id = ?1", nativeQuery = true)
+    List<Report> findAllByProfile_id(Long id);
 }
